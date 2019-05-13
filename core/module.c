55dfb6cf80a44873975a61dc13fa0ff6e0104e10
Dracula1998 <1446953337@qq.com>
2019-05-14 00:02:31 +0800

    Update module.c

--- core/module.c
+++ core/module.c
@@ -7,6 +7,11 @@
 #include <linux/udp.h>
 #include <linux/skbuff.h>
 #include "constants.h"
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/timer.h>
+#include <linux/timex.h>
+#include <linux/string.h>
 
 //========================Filter Declaration==START==Author: @wzs82868996==================
 bool check_tcp(struct iphdr *ip, struct tcphdr *tcp, unsigned char *data, int length);
@@ -17,7 +22,23 @@ bool check_udp(struct iphdr *ip, struct udphdr *udp, unsigned char *data, int le
 
 
 //========================Logger Declaration==START==Author: @Dracula1998==================
+#define DEBUG -1
+#define OK 0
+#define LOW 1
+#define WARN 2
+#define FATAL 3
 
+struct {
+    char *time;
+    char *source;
+    char *message;
+    int level;
+    char *level_str;
+} logger_record;
+
+void init_writer();
+int log();
+void close_writer();
 
 //========================Logger Declaration==END==========================================
 
@@ -125,6 +146,125 @@ bool check_udp(struct iphdr *ip, struct udphdr *udp, unsigned char *data, int le
 
 
 //========================Logger Implementation==START==Author: @Dracula1998===============
+
+struct file *fp;
+time_t tt;
+tm* t;
+char time_str_space[50];
+char log_space[400];
+
+void init_writer()
+{
+    fp = filp_open("/var/log/NetFilter.log",O_RDWR | O_CREAT,0644);
+    if (IS_ERR(fp)){
+        printk("create file error/n");
+        return -1;
+    }
+}
+
+void write_log(char *log_str)
+{
+    mm_segment_t fs;
+    loff_t pos;
+
+    printk("hello enter/n");
+
+    fs = get_fs();
+    set_fs(KERNEL_DS);
+    pos = file_pos_read(file)
+    vfs_write(fp, log_str, strlen(log_str)*8, &pos);
+    vfs_read(fp, buf1, sizeof(buf), &pos);
+    file_pos_write(file, pos);
+    printk("read: %s/n",buf1);
+    set_fs(fs);
+    return 0;
+}
+
+void close_writer()
+{
+    filp_close(fp,NULL);
+}
+
+char *get_current_time()
+{
+    tt = time(NULL);
+	t = localtime(&tt);
+	char *log_time = time_str_space;
+	log_time = "";
+	log_time = strcat(log_time, t->tm_year + 1900);
+	log_time = strcat(log_time, "-");
+	log_time = strcat(log_time, t->tm_mon + 1);
+	log_time = strcat(log_time, "-");
+	log_time = strcat(log_time, t->tm_mday);
+	log_time = strcat(log_time, " ");
+	log_time = strcat(log_time, t->tm_hour);
+	log_time = strcat(log_time, ":");
+	log_time = strcat(log_time, t->tm_min);
+	log_time = strcat(log_time, ":");
+	log_time = strcat(log_time, t->tm_sec);
+
+    return log_time;
+	/*
+	printf("\n%d-%02d-%02d %02d:%02d:%02d\n",
+		t->tm_year + 1900,
+		t->tm_mon + 1,
+		t->tm_mday,
+		t->tm_hour,
+		t->tm_min,
+		t->tm_sec);
+    */
+}
+
+int log() {
+    logger_record.time = get_current_time();
+    printk(logger_record.source);
+    printk(logger_record.message);
+    printk("%d", logger_record.level);
+    switch(logger_record.level)
+    {
+    case -1:
+        logger_record.level_str = "DEBUG";
+        break;
+    case 0:
+        logger_record.level_str = "OK";
+        break;
+    case 1:
+        logger_record.level_str = "LOW";
+        break;
+    case 2:
+        logger_record.level_str = "WARN";
+        break;
+    case 3:
+        logger_record.level_str = "FATAL";
+        break;
+    default:
+        logger_record.level_str = "UNKNOWN";
+        break;
+    }
+    char *log_str = log_space;
+    log_str = logger_record.time;
+    log_str = strcat(log_str, " [");
+    log_str = strcat(log_str, logger_record.level_str);
+    log_str = strcat(log_str, "][");
+    log_str = strcat(log_str, logger_record.source);
+    log_str = strcat(log_str, "] ");
+    log_str = strcat(log_str, logger_record.message);
+    log_str = strcat(log_str, "\n");
+
+    write_log(log_str);
+    return 0;
+}
+
+
+int main(void) {
+    logger_record.source = "source";
+    logger_record.message = "message";
+    logger_record.level = DEBUG;
+    init_writer();
+    log();
+    close_writer();
+    return 0;
+}
 
 
 //========================Logger Implementation==END=======================================
