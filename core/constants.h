a636f0c7f8355c5dddfb9805a18e1866bfa10d2e
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-05-22 01:42:41 +0800

    add console color support

--- core/constants.h
+++ core/constants.h
@@ -12,10 +12,88 @@
 #define NAME "[Netfilter Firewall] "
 
 // ==== logger level ======
-#define LOGGER_DEBUG -1
-#define LOGGER_OK 0
-#define LOGGER_LOW 1
-#define LOGGER_WARN 2
-#define LOGGER_FATAL 3
+
+#define LOGGER_DEBUG (-1)
+#define LOGGER_INFO 0
+#define LOGGER_OK 1
+#define LOGGER_LOW 2
+#define LOGGER_WARN 3
+#define LOGGER_FATAL 4
+
+
+// ==== console color ====
+
+// Reset
+#define COLOR_RESET "\033[0m";  // Text Reset
+
+// Regular Colors
+#define COLOR_BLACK "\033[0;30m";   // BLACK
+#define COLOR_RED "\033[0;31m";     // RED
+#define COLOR_GREEN "\033[0;32m";   // GREENK
+#define COLOR_YELLOW "\033[0;33m";  // YELLOW
+#define COLOR_BLUE "\033[0;34m";    // BLUE
+#define COLOR_PURPLE "\033[0;35m";  // PURPLE
+#define COLOR_CYAN "\033[0;36m";    // CYAN
+#define COLOR_WHITE "\033[0;37m";   // WHITE
+
+// Bold
+#define COLOR_BLACK_BOLD "\033[1;30m";  // BLACK
+#define COLOR_RED_BOLD "\033[1;31m";    // RED
+#define COLOR_GREEN_BOLD "\033[1;32m";  // GREEN
+#define COLOR_YELLOW_BOLD "\033[1;33m"; // YELLOW
+#define COLOR_BLUE_BOLD "\033[1;34m";   // BLUE
+#define COLOR_PURPLE_BOLD "\033[1;35m"; // PURPLE
+#define COLOR_CYAN_BOLD "\033[1;36m";   // CYAN
+#define COLOR_WHITE_BOLD "\033[1;37m";  // WHITE
+
+// Underline
+#define COLOR_BLACK_UNDERLINED "\033[4;30m";  // BLACK
+#define COLOR_RED_UNDERLINED "\033[4;31m";    // RED
+#define COLOR_GREEN_UNDERLINED "\033[4;32m";  // GREEN
+#define COLOR_YELLOW_UNDERLINED "\033[4;33m"; // YELLOW
+#define COLOR_BLUE_UNDERLINED "\033[4;34m";   // BLUE
+#define COLOR_PURPLE_UNDERLINED "\033[4;35m"; // PURPLE
+#define COLOR_CYAN_UNDERLINED "\033[4;36m";   // CYAN
+#define COLOR_WHITE_UNDERLINED "\033[4;37m";  // WHITE
+
+// Background
+#define COLOR_BLACK_BACKGROUND "\033[40m";  // BLACK
+#define COLOR_RED_BACKGROUND "\033[41m";    // RED
+#define COLOR_GREEN_BACKGROUND "\033[42m";  // GREEN
+#define COLOR_YELLOW_BACKGROUND "\033[43m"; // YELLOW
+#define COLOR_BLUE_BACKGROUND "\033[44m";   // BLUE
+#define COLOR_PURPLE_BACKGROUND "\033[45m"; // PURPLE
+#define COLOR_CYAN_BACKGROUND "\033[46m";   // CYAN
+#define COLOR_WHITE_BACKGROUND "\033[47m";  // WHITE
+
+// High Intensity
+#define COLOR_BLACK_BRIGHT "\033[0;90m";  // BLACK
+#define COLOR_RED_BRIGHT "\033[0;91m";    // RED
+#define COLOR_GREEN_BRIGHT "\033[0;92m";  // GREEN
+#define COLOR_YELLOW_BRIGHT "\033[0;93m"; // YELLOW
+#define COLOR_BLUE_BRIGHT "\033[0;94m";   // BLUE
+#define COLOR_PURPLE_BRIGHT "\033[0;95m"; // PURPLE
+#define COLOR_CYAN_BRIGHT "\033[0;96m";   // CYAN
+#define COLOR_WHITE_BRIGHT "\033[0;97m";  // WHITE
+
+// Bold High Intensity
+#define COLOR_BLACK_BOLD_BRIGHT "\033[1;90m"; // BLACK
+#define COLOR_RED_BOLD_BRIGHT "\033[1;91m";   // RED
+#define COLOR_GREEN_BOLD_BRIGHT "\033[1;92m"; // GREEN
+#define COLOR_YELLOW_BOLD_BRIGHT "\033[1;93m";// YELLOW
+#define COLOR_BLUE_BOLD_BRIGHT "\033[1;94m";  // BLUE
+#define COLOR_PURPLE_BOLD_BRIGHT "\033[1;95m";// PURPLE
+#define COLOR_CYAN_BOLD_BRIGHT "\033[1;96m";  // CYAN
+#define COLOR_WHITE_BOLD_BRIGHT "\033[1;97m"; // WHITE
+
+// High Intensity backgrounds
+#define COLOR_BLACK_BACKGROUND_BRIGHT "\033[0;100m";// BLACK
+#define COLOR_RED_BACKGROUND_BRIGHT "\033[0;101m";// RED
+#define COLOR_GREEN_BACKGROUND_BRIGHT "\033[0;102m";// GREEN
+#define COLOR_YELLOW_BACKGROUND_BRIGHT "\033[0;103m";// YELLOW
+#define COLOR_BLUE_BACKGROUND_BRIGHT "\033[0;104m";// BLUE
+#define COLOR_PURPLE_BACKGROUND_BRIGHT "\033[0;105m"; // PURPLE
+#define COLOR_CYAN_BACKGROUND_BRIGHT "\033[0;106m";  // CYAN
+#define COLOR_WHITE_BACKGROUND_BRIGHT "\033[0;107m";   // WHITE
 
 #endif //FIREWALL_CONSTANTS_H
