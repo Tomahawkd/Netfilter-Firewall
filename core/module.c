ddfa3d745d78e863e6a9426f2ca7af55c5a659fd
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-05-26 16:26:01 +0800

    rearrange for unwanted usage

--- core/module.c
+++ core/module.c
@@ -56,106 +56,10 @@ void close_writer(void);
 //========================Messager Declaration==END========================================
 
 
+//========================Messager Implementation==START==Author: @Vshows==================
 
-//========================Kernel Module Implementation==START==Author: @Tomahawkd==========
-static struct nf_hook_ops nfho;
 
-unsigned int hook_funcion(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-
-    if (!skb) return NF_ACCEPT;
-
-    char info[256];
-
-    struct iphdr *ip = ip_hdr(skb);
-    if (!ip) return NF_ACCEPT;
-    unsigned int saddr = ip->saddr;
-    unsigned int daddr = ip->daddr;
-
-    sprintf(info, "IP[%u.%u.%u.%u]--->[%u.%u.%u.%u]", saddr & 255u, saddr >> 8u & 255u, saddr >> 16u & 255u,
-           saddr >> 24u & 255u, daddr & 255u, daddr >> 8u & 255u, daddr >> 16u & 255u, daddr >> 24u & 255u);
-    log_message("Hook Function IP", LOGGER_OK, info);
-
-    if (ip->protocol == IPPROTO_TCP) {
-        struct tcphdr *tcp = tcp_hdr(skb);
-
-        sprintf(info, "TCP[%u.%u.%u.%u:%hu]-->[%u.%u.%u.%u:%hu]", saddr & 255u, saddr >> 8u & 255u,
-                saddr >> 16u & 255u, saddr >> 24u & 255u, tcp->source, daddr & 255u, daddr >> 8u & 255u,
-               daddr >> 16u & 255u, daddr >> 24u & 255u, tcp->dest);
-        log_message("Hook Function TCP", LOGGER_OK, info);
-
-        unsigned char *user_data = (unsigned char *) ((unsigned char *) tcp + (tcp->doff * 4));
-        unsigned char *tail = skb_tail_pointer(skb);
-        if (user_data && tail) {
-            int datasize = (int) ((long) tail - (long) user_data);
-
-            if (datasize > 0 && check_tcp(ip, tcp, user_data, datasize)) {
-                return NF_ACCEPT;
-            } else {
-                return NF_ACCEPT;
-            }
-        }
-
-    } else if (ip->protocol == IPPROTO_UDP) {
-        struct udphdr *udp = udp_hdr(skb);
-
-        sprintf(info, "UDP[%u.%u.%u.%u:%hu]-->[%u.%u.%u.%u:%hu]", saddr & 255u, saddr >> 8u & 255u,
-                saddr >> 16u & 255u, saddr >> 24u & 255u, udp->source, daddr & 255u, daddr >> 8u & 255u,
-               daddr >> 16u & 255u, daddr >> 24u & 255u, udp->dest);
-        log_message("Hook Function UDP", LOGGER_OK, info);
-
-        unsigned char *user_data = (unsigned char *) ((unsigned char *) udp + 32);
-        if (user_data) {
-            int datasize = udp->len;
-
-            if (datasize > 0 && check_udp(ip, udp, user_data, datasize)) {
-                return NF_ACCEPT;
-            } else {
-                return NF_ACCEPT;
-            }
-        }
-    } else {
-        return NF_ACCEPT;
-    }
-
-    return NF_ACCEPT;
-}
-
-static int __init hook_init(void) {
-    int ret = 0;
-    struct net *n;
-
-    init_writer();
-
-    nfho.hook = hook_funcion;
-    nfho.pf = NFPROTO_IPV4;
-    nfho.hooknum = NF_INET_PRE_ROUTING;
-    nfho.priority = NF_IP_PRI_MANGLE;
-    for_each_net(n)ret += nf_register_net_hook(n, &nfho);
-
-    char message[128];
-    sprintf(message, "nf_register_hook returnd %d", ret);
-    log_message("Hook init", LOGGER_OK, message);
-
-    return 0;
-}
-
-static void __exit hook_exit(void) {
-    struct net *n;
-
-    log_message("Hook exit", LOGGER_OK, "Hook deinit");
-
-    for_each_net(n)nf_unregister_net_hook(n, &nfho);
-
-    close_writer();
-}
-
-module_init(hook_init)
-module_exit(hook_exit)
-
-MODULE_LICENSE("Dual MIT/GPL");
-MODULE_AUTHOR("Tomahawkd");
-//========================Kernel Module Implementation==END================================
-
+//========================Messager Implementation==END=====================================
 
 
 //========================Filter Implementation==START==Author: @wzs82868996===============
@@ -310,9 +214,101 @@ void log_message(char *source, int level, char *message) {
 //========================Logger Implementation==END=======================================
 
 
+//========================Kernel Module Implementation==START==Author: @Tomahawkd==========
+static struct nf_hook_ops nfho;
 
+unsigned int hook_funcion(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
 
-//========================Messager Implementation==START==Author: @Vshows==================
+    if (!skb) return NF_ACCEPT;
 
+    char info[256];
 
-//========================Messager Implementation==END=====================================
+    struct iphdr *ip = ip_hdr(skb);
+    if (!ip) return NF_ACCEPT;
+    unsigned int saddr = ip->saddr;
+    unsigned int daddr = ip->daddr;
+
+    sprintf(info, "IP[%u.%u.%u.%u]--->[%u.%u.%u.%u]", saddr & 255u, saddr >> 8u & 255u, saddr >> 16u & 255u,
+            saddr >> 24u & 255u, daddr & 255u, daddr >> 8u & 255u, daddr >> 16u & 255u, daddr >> 24u & 255u);
+    log_message("Hook Function IP", LOGGER_OK, info);
+
+    if (ip->protocol == IPPROTO_TCP) {
+        struct tcphdr *tcp = tcp_hdr(skb);
+
+        sprintf(info, "TCP[%u.%u.%u.%u:%hu]-->[%u.%u.%u.%u:%hu]", saddr & 255u, saddr >> 8u & 255u,
+                saddr >> 16u & 255u, saddr >> 24u & 255u, tcp->source, daddr & 255u, daddr >> 8u & 255u,
+                daddr >> 16u & 255u, daddr >> 24u & 255u, tcp->dest);
+        log_message("Hook Function TCP", LOGGER_OK, info);
+
+        unsigned char *user_data = (unsigned char *) ((unsigned char *) tcp + (tcp->doff * 4));
+        unsigned char *tail = skb_tail_pointer(skb);
+        if (user_data && tail) {
+            int datasize = (int) ((long) tail - (long) user_data);
+
+            if (datasize > 0 && check_tcp(ip, tcp, user_data, datasize)) {
+                return NF_ACCEPT;
+            } else {
+                return NF_ACCEPT;
+            }
+        }
+
+    } else if (ip->protocol == IPPROTO_UDP) {
+        struct udphdr *udp = udp_hdr(skb);
+
+        sprintf(info, "UDP[%u.%u.%u.%u:%hu]-->[%u.%u.%u.%u:%hu]", saddr & 255u, saddr >> 8u & 255u,
+                saddr >> 16u & 255u, saddr >> 24u & 255u, udp->source, daddr & 255u, daddr >> 8u & 255u,
+                daddr >> 16u & 255u, daddr >> 24u & 255u, udp->dest);
+        log_message("Hook Function UDP", LOGGER_OK, info);
+
+        unsigned char *user_data = (unsigned char *) ((unsigned char *) udp + 32);
+        if (user_data) {
+            int datasize = udp->len;
+
+            if (datasize > 0 && check_udp(ip, udp, user_data, datasize)) {
+                return NF_ACCEPT;
+            } else {
+                return NF_ACCEPT;
+            }
+        }
+    } else {
+        return NF_ACCEPT;
+    }
+
+    return NF_ACCEPT;
+}
+
+static int __init hook_init(void) {
+    int ret = 0;
+    struct net *n;
+
+    init_writer();
+
+    nfho.hook = hook_funcion;
+    nfho.pf = NFPROTO_IPV4;
+    nfho.hooknum = NF_INET_PRE_ROUTING;
+    nfho.priority = NF_IP_PRI_MANGLE;
+    for_each_net(n)ret += nf_register_net_hook(n, &nfho);
+
+    char message[128];
+    sprintf(message, "nf_register_hook returnd %d", ret);
+    log_message("Hook init", LOGGER_OK, message);
+
+    return 0;
+}
+
+static void __exit hook_exit(void) {
+    struct net *n;
+
+    log_message("Hook exit", LOGGER_OK, "Hook deinit");
+
+    for_each_net(n)nf_unregister_net_hook(n, &nfho);
+
+    close_writer();
+}
+
+module_init(hook_init)
+module_exit(hook_exit)
+
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_AUTHOR("Tomahawkd");
+//========================Kernel Module Implementation==END================================
